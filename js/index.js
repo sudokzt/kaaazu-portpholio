0ef1f3deb345104df6f035c3072dac4953a4f9d0
k.mishima <k.mishima@kyotoinnovation.jp>
2019-04-25 14:09:34 +0900

    ダイアログ修正

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const dialog = document.getElementById("skills__dialog");
+window.onload = dialog.close();
 
 document.getElementById("skills__dialog-open").addEventListener("click", () => {
   dialog.showModal();
